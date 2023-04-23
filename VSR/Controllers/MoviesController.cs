f607980bddb567a594b853a96f99d0edd432de1a
preetham-coupa <preetham.p-m@coupa.com>
2023-04-23 15:21:56 +0530

    first commit

---
+++ VSR/Controllers/MoviesController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VSR.Models;
+using VSR.ViewModel.Movies;
+
+namespace VSR.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        private MyDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new MyDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.ToList();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return Content("Page not found");
+
+            return View(movie);
+
+        }
+        // Random Movie Page
+        public ActionResult Random()
+        {
+            var movie = new Movie{ Name="F1" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="C1" },
+                new Customer{Name="C2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ VSR/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VSR.Migrations;
+
+namespace VSR.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte NumberInStock { get; set; }
+    }
+}
